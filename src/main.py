7d2f8407a883fa93c5c55a4538aafd898c154a00
qiudeng7 <2143975743@qq.com>
2024-09-01 10:19:59 +0800

    reading: adjust main.py format and comment

--- src/main.py
+++ src/main.py
@@ -7,22 +7,22 @@ from PageCrawler.BasePageCrawler import BasePageCrawler
 
 
 def main():
-
+    # get args
     parser = argparse.ArgumentParser(description='douyin crawler program.')
     parser.add_argument(
         '--page_type', type=str, help='douyin page type, select ( video | user )',
                         choices=["video","user"],default="video")
     
+    # setup database
     database.get().setup_table_schema()
     
+    # launch crawler
     page_type = parser.parse_args().page_type
     pageCrawlers:dict[BasePageCrawler] = {
         'video':videoPageCrawler,
         'user':userPageCrawler
     }
-    
-    crawler: BasePageCrawler = pageCrawlers[page_type]
-    
+    crawler = pageCrawlers[page_type]
     crawler()
 
 
